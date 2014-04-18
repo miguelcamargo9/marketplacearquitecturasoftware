ea33f0f7507ee9cf2523ad118827e6b5d0d9913a
azulsebas123@gmail.com <azulsebas123@gmail.com@61d57201-5a28-e573-ab49-4046894d42d1>
2014-04-18 02:39:04 +0000

    cambios sebastian

---
+++ branches/marketplaceEJB/marketplaceEJB-war/src/java/com/marketPlace/servlets/validarUsuarioServlet.java
@@ -0,0 +1,114 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.marketPlace.servlets;
+
+import com.marketplace.session.UsuariosFacade;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Sebastian Rojas
+ */
+@WebServlet(name = "validarUsuarioServlet", urlPatterns = {"/validarUsuarioServlet"})
+public class validarUsuarioServlet extends HttpServlet {
+  @EJB
+  private UsuariosFacade usuariosFacade;
+
+  String error = "";
+  boolean bandera = true;
+  
+
+  /**
+   * Processes requests for both HTTP
+   * <code>GET</code> and
+   * <code>POST</code> methods.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    try {
+      if (bandera) {
+        HttpSession session = request.getSession();
+        session.setAttribute("exito", "");
+        session.setAttribute("error", "");
+        if (session != null) {
+          session.setAttribute("exito", "Se acepto con exito al cliente");
+          response.sendRedirect("vistas/validarCliente.jsp");
+        }
+      } else {
+        HttpSession session = request.getSession();
+        if (session != null) {
+          session.setAttribute("error", error);
+          response.sendRedirect("vistas/validarCliente.jsp");
+        }
+      }
+    } finally {
+      out.close();
+    }
+  }
+
+  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  /**
+   * Handles the HTTP
+   * <code>GET</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP
+   * <code>POST</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+
+    if (request.getParameter("idUsuario").equals("") || request.getParameter("idUsuario") == null) {
+      error += "Por favor seleccione un usuario...<br>";
+      bandera = false;
+    } else {
+      int idUsuario = Integer.parseInt(request.getParameter("idUsuario"));
+      usuariosFacade.actualizarInformacionUsuario(idUsuario);
+    }
+    processRequest(request, response);
+  }
+
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo() {
+    return "Short description";
+  }// </editor-fold>
+}
