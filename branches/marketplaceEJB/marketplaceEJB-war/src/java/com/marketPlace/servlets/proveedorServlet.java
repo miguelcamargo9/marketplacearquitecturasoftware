9b8f0395663309774606bbfa12e618e7bdf98ce6
miguelcamargo9@gmail.com <miguelcamargo9@gmail.com@61d57201-5a28-e573-ab49-4046894d42d1>
2014-04-27 21:12:27 +0000

    cambios para la creación del proveedor

--- branches/marketplaceEJB/marketplaceEJB-war/src/java/com/marketPlace/servlets/proveedorServlet.java
+++ branches/marketplaceEJB/marketplaceEJB-war/src/java/com/marketPlace/servlets/proveedorServlet.java
@@ -13,7 +13,6 @@ import java.io.PrintWriter;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,17 +20,16 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author Miguel
+ * @author Sebastian Rojas
  */
-@WebServlet(name = "registroServlet", urlPatterns = {"/registroServlet"})
-public class registroServlet extends HttpServlet {
+@WebServlet(name = "proveedorServlet", urlPatterns = {"/proveedorServlet"})
+public class proveedorServlet extends HttpServlet {
   @EJB
   private UsuariosFacade usuariosFacade;
-
+  
   Usuarios usuario;
   private boolean bandera;
   private String error = "";
-  
   /**
    * Processes requests for both HTTP
    * <code>GET</code> and
@@ -47,16 +45,17 @@ public class registroServlet extends HttpServlet {
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
     try {
+      /* TODO output your page here. You may use following sample code. */
       if (bandera) {
         HttpSession session = request.getSession();
-        session.setAttribute("mensaje", ""+usuario.getPrimerNombre()+" Tu solicitud ha sido creado con Exito!");
+        session.setAttribute("mensaje", ""+usuario.getPrimerNombre()+" Tu registro ha sido creado con Exito!");
         session.setMaxInactiveInterval(1);
-        response.sendRedirect("index.jsp");
+        response.sendRedirect("vistas/crearProveedores.jsp");
       } else {
         HttpSession session = request.getSession();
         session.setAttribute("error", error);
         session.setMaxInactiveInterval(1);
-        response.sendRedirect("vistas/registrarseVista.jsp");
+        response.sendRedirect("vistas/crearProveedores.jsp");
       }
     } finally {      
       out.close();
@@ -91,35 +90,30 @@ public class registroServlet extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
           throws ServletException, IOException {
-    
-    String idUsuario = request.getParameter("idUsuario");
+    String nitProveedor = request.getParameter("nitProveedor");
     String nickname = request.getParameter("nickname");
-    String primerNombre = request.getParameter("primerNombre");
-    String segundoNombre = request.getParameter("segundoNombre");
-    String primerApellido = request.getParameter("primerApellido");
-    String segundoApellido = request.getParameter("segundoApellido");
+    String razonSocial = request.getParameter("razonSocial");
     String passwordMd5 = request.getParameter("password");
     String passwordMd5C = request.getParameter("passwordC");
     String correo = request.getParameter("correo");
     usuario = usuariosFacade.getBuscarInfoUser(nickname);
+    
     if (usuario == null && passwordMd5.equals(passwordMd5C)) {
       bandera = true;
-      usuario = new Usuarios(Integer.parseInt(idUsuario),nickname,passwordMd5,true);
+      usuario = new Usuarios(Integer.parseInt(nitProveedor),nickname,passwordMd5,true);
       usuario.setIdPerfil(2);
-      usuario.setPrimerNombre(primerNombre);
-      usuario.setPrimerApellido(primerApellido);
-      usuario.setSegundoApellido(segundoApellido);
-      usuario.setSegundoNombre(segundoNombre);
+      usuario.setNombreComercial(razonSocial);
       usuario.setCorreo(correo);
       usuariosFacade.setUsuario(this.usuario);
     } else {
       error += usuario != null ? "El usuario ya existe <br>" : "";
-      error += idUsuario.equals("") ? "Por favor ingrese su Cedula <br>" : "";
+      error += nitProveedor.equals("") ? "Por favor ingrese su Nit <br>" : "";
       error += nickname.equals("") ? "Por favor ingrese su nickname <br>" : "";
       error += passwordMd5.equals("") ? "Por favor ingrese su contraseña <br>" : "";
       error += !passwordMd5.equals(passwordMd5C) ? "Las contraseñas no coinciden Verifique <br>" : "";
       bandera = false;
     }
+    
     processRequest(request, response);
   }
 
